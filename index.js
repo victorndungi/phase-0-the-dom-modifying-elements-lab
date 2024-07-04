fb1377e2f484e4eb276f579e509163f2add80ad8
victorndungi <victor.ndungi@student.moringaschool.com>
2024-07-05 00:05:13 +0300

    add index,js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.body.removeChild(main)
+
+const newHeader=document.createElement('h1')
+newHeader.id='victory'
+document.body.appendChild(newHeader)
+newHeader.innerHTML='is the champion'
